12841fe1726bb4fb9e53bfbfb50acd33d48605b7
Tanyajha-2003 <tanyakumar2003@gmail.com>
2025-12-05 20:46:36 +0530

    Fixed frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,14 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+  origin: [
+    'http://localhost:5173', 
+    'https://your-frontend-on-render.com' 
+  ],
+  credentials: true,
+});
+
   const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`Server listening on ${port}`);
